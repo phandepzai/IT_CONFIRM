0a2759809df3456bdc70f4c565aaa1386c600058
nongvanphan <60602558+phandepzai@users.noreply.github.com>
2025-10-26 19:05:15 +0700

    Add files via upload

--- IT CONFIRM/Properties/AssemblyInfo.cs
+++ IT CONFIRM/Properties/AssemblyInfo.cs
@@ -29,5 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("2.0.0.4")]
+[assembly: AssemblyFileVersion("2.0.0.4")]
+
